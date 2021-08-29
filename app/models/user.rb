1f442d477aef55c76d880274a53a5b376fb005dc
Narumi2233 <lunarianoce@gmail.com>
2021-08-29 16:57:30 +0900

    バリデーション、ビュー編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :encrypted_password, with: PASSWORD_REGEX
+
+         validates :nickname, presence: true
+         validates :encrypted_password, presence: true,confirmation: true
+         validates :last_name, presence: true
+         validates :first_name, presence: true
+         validates :last_name_kana, presence: true
+         validates :first_name_kana, presence: true
+         validates :birthday, presence: true
 end
