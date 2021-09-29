5932a5fb6e06c240642de566fba78102ddffc98b
Narumi2233 <lunarianoce@gmail.com>
2021-09-29 19:29:20 +0900

    コードレビュー前修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-         validates_format_of :password, with: PASSWORD_REGEX,message: "は英数字混合にしてください"
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'は英数字混合にしてください'
 
-         validates :nickname, presence: true
-         validates :encrypted_password, presence: true,confirmation: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_name_kana, presence: true
-         validates :first_name_kana, presence: true
-         validates :birthday, presence: true
+  validates :nickname, presence: true
+  validates :encrypted_password, presence: true, confirmation: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
 end
