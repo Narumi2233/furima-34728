01600fe39c4d3eab8949b08682f40d13abf67851
Narumi2233 <lunarianoce@gmail.com>
2021-09-29 18:43:57 +0900

    モデルの単体テスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name {'test'}
+    first_name{'mei'}
+    last_name_kana{'テスト'}
+    first_name_kana{'メイ'}
+    birthday{"2000-01-01"}
+  end
+end
