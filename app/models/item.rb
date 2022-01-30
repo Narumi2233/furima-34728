ceb79576024c5cd9f0d1b2a07f2af08bfb612d76
Narumi2233 <lunarianoce@gmail.com>
2022-01-30 16:22:47 +0900

    モデルの移動

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,9 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
-
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :delivery_days
 end
