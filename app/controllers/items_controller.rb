372b338622423ed8329d18d0af3af6231126c8b5
Narumi2233 <lunarianoce@gmail.com>
2022-05-15 00:27:27 +0900

    ルーティング、アクション定義、カラムの指定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, except: [:index,:show]
-  
+  before_action :baria_user, only: [:edit, :update]
+
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -23,10 +24,29 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+   if @item.update(item_params)
+    redirect_to item_path(@item.id)
+   else
+    render :edit
+   end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image , :title, :explanation, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :delivery_days_id, :price ).merge(user_id: current_user.id)
   end
+
+  def baria_user
+    unless Item.find(params[:id]).user.id.to_i == current_user.id
+        redirect_to root_path
+    end
+   end
 
 end
