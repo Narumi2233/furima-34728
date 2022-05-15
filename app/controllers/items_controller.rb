360de8ee18c39fd4a9c2fc45570924fa34fb0f07
Narumi2233 <lunarianoce@gmail.com>
2022-05-16 00:52:44 +0900

    修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
 
   before_action :authenticate_user!, except: [:index,:show]
   before_action :baria_user, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -12,7 +13,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def create
@@ -25,11 +25,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
    if @item.update(item_params)
     redirect_to item_path(@item.id)
    else
@@ -48,5 +46,9 @@ class ItemsController < ApplicationController
         redirect_to root_path
     end
    end
+
+   def set_item
+    @item = Item.find(params[:id])
+  end
 
 end
