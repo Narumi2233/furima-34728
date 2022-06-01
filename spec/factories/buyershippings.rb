34662788a2c9dd040b6c1231a6685625d81fbf78
Narumi2233 <lunarianoce@gmail.com>
2022-06-01 16:31:54 +0900

    テストファイル導入、条件文訂正

---
+++ spec/factories/buyershippings.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :buyer_shipping do
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { '1q2w3e' }
+    password_confirmation { password }
+    last_name { 'てすと' }
+    first_name { 'めい' }
+    last_name_kana { 'テスト' }
+    first_name_kana { 'メイ' }
+    birthday { '2000-01-01' }
+  end
+end
